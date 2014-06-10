54182d2f222279b235feddfb8ca1be9cb4b4a924
Jayis <jayis4176@gmail.com>
2014-06-11 03:20:18 +0800

    miss one

--- test.m
+++ test.m
@@ -1,11 +1,11 @@
 input = 5;
-target = 4;
+target = 3;
 
-wObj=waveFile2obj('cut.wav');
+wObj=waveFile2obj('test.wav');
 opt=pitchShift('defaultOpt');
 opt.method='wsola';
 opt.pitchShiftAmount = log2(target/input) * 12;
 wObj2=pitchShift(wObj, opt, 1);
 %[n,d] = rat(2);
 wObj2.signal = resample(wObj2.signal, target, input);
-audiowrite('cut_double.wav', wObj2.signal, wObj.fs);
+audiowrite('test_out1.wav', wObj2.signal, wObj.fs);
